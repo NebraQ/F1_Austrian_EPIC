5264a57d0b739e69bb1bfdb56c006013e43fdc05
NebraQ <87468648+NebraQ@users.noreply.github.com>
2025-11-19 11:53:14 +0100

    Fix duplicate entry for Suzuka and add Placeholder

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ const tracks = [
     { id:21, name:"Sakhir", main1:"Reifenmanagement", main2:"Antrieb", img:"21_Sakhir.png" },
     { id:22, name:"Barcelona", main1:"Reifenmanagement", main2:"Kurvenverhalten", img:"22_Barcelona.png" },
     { id:23, name:"Suzuka", main1:"Verteidigen", main2:"Kurvenverhalten", img:"23_Suzuka.png" }
-    { id:23, name:"Suzuka", main1:"Verteidigen", main2:"Kurvenverhalten", img:"23_Suzuka.png" }
+    { id:24, name:"Placeholder", main1:"-", main2:"-"}
 ];
 
 
